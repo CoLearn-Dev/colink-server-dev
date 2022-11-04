06392c1af7750c4a2c7034a819ebdd100b73650a
stneng <git@stneng.com>
2022-11-04 00:39:55 +0000

    default CLI params, env fallback

--- src/main.rs
+++ src/main.rs
@@ -7,67 +7,75 @@ use structopt::StructOpt;
 struct CommandLineArgs {
     /// Address of CoLink server
     // short and long flags (-d, --debug) will be deduced from the field's name
-    #[structopt(short, long)]
+    #[structopt(short, long, env = "COLINK_SERVER_ADDRESS", default_value = "0.0.0.0")]
     address: String,
 
     /// Port of CoLink server
-    #[structopt(short, long)]
+    #[structopt(short, long, env = "COLINK_SERVER_PORT", default_value = "8080")]
     port: u16,
 
     /// AMQP URI of MQ
-    #[structopt(long)]
+    #[structopt(
+        long,
+        env = "COLINK_SERVER_MQ_AMQP",
+        default_value = "amqp://guest:guest@localhost:5672"
+    )]
     mq_amqp: String,
 
     /// Management API of MQ
-    #[structopt(long)]
+    #[structopt(
+        long,
+        env = "COLINK_SERVER_MQ_API",
+        default_value = "http://guest:guest@localhost:15672/api"
+    )]
     mq_api: String,
 
     /// Prefix of MQ
-    #[structopt(long, default_value = "colink")]
+    #[structopt(long, env = "COLINK_SERVER_MQ_PREFIX", default_value = "colink")]
     mq_prefix: String,
 
     /// URI of CoLink server
-    #[structopt(short, long)]
+    #[structopt(long, env = "COLINK_SERVER_CORE_URI")]
     core_uri: Option<String>,
 
     /// Path to server certificate.
     /// If not supplied, TLS and MTLS will not be enabled
-    #[structopt(long, parse(from_os_str))]
+    #[structopt(long, parse(from_os_str), env = "COLINK_SERVER_CERT")]
     cert: Option<PathBuf>,
 
     /// Path to private key.
     /// If not supplied, TLS and MTLS will not be enabled
-    #[structopt(long, parse(from_os_str))]
+    #[structopt(long, parse(from_os_str), env = "COLINK_SERVER_KEY")]
     key: Option<PathBuf>,
 
     /// Path to client CA certificate.
     /// If not supplied and both cert and key are supplied, then TLS is enabled.
     /// If all of cert, key, and ca are supplied, then MTLS is enabled.
-    #[structopt(long, parse(from_os_str))]
+    #[structopt(long, parse(from_os_str), env = "COLINK_SERVER_CA")]
     ca: Option<PathBuf>,
 
     /// Path to CA certificate you want to trust in inter-core communication.
-    #[structopt(long, parse(from_os_str))]
+    #[structopt(long, parse(from_os_str), env = "COLINK_SERVER_INTER_CORE_CA")]
     inter_core_ca: Option<PathBuf>,
 
     /// Path to client certificate you want to use in inter-core communication.
-    #[structopt(long, parse(from_os_str))]
+    #[structopt(long, parse(from_os_str), env = "COLINK_SERVER_INTER_CORE_CERT")]
     inter_core_cert: Option<PathBuf>,
 
     /// Path to private key you want to use in inter-core communication.
-    #[structopt(long, parse(from_os_str))]
+    #[structopt(long, parse(from_os_str), env = "COLINK_SERVER_INTER_CORE_KEY")]
     inter_core_key: Option<PathBuf>,
 
     /// Enforce the generation of the new jwt secret.
-    #[structopt(long)]
+    #[structopt(long, env = "COLINK_SERVER_FORCE_GEN_JWT_SECRET")]
     force_gen_jwt_secret: bool,
 
     /// Enforce the generation of the core's private key.
-    #[structopt(long)]
+    #[structopt(long, env = "COLINK_SERVER_FORCE_GEN_PRIV_KEY")]
     force_gen_priv_key: bool,
 
     /// Create reverse connections to other servers.
-    #[structopt(long)]
+    #[structopt(long, env = "COLINK_SERVER_INTER_CORE_REVERSE_MODE")]
     inter_core_reverse_mode: bool,
 }
 
