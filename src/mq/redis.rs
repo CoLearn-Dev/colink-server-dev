f398b57b0272063733c35df3096421bf2d4bf33c
stneng <git@stneng.com>
2023-01-30 03:53:11 +0000

    mq: redis user

--- src/mq/redis.rs
+++ src/mq/redis.rs
@@ -1,4 +1,5 @@
 use redis::{
+    acl::Rule,
     aio::Connection,
     streams::{StreamReadOptions, StreamReadReply},
     AsyncCommands, FromRedisValue,
@@ -25,19 +26,98 @@ impl RedisStream {
 #[async_trait::async_trait]
 impl crate::mq::common::MQ for RedisStream {
     async fn create_user_account(&self) -> Result<String, String> {
-        Ok(self.redis_uri.clone())
+        let ug = passwords::PasswordGenerator::new()
+            .length(16)
+            .numbers(true)
+            .lowercase_letters(true);
+        let pg = passwords::PasswordGenerator::new()
+            .length(32)
+            .numbers(true)
+            .lowercase_letters(true)
+            .uppercase_letters(true);
+        let username = ug.generate_one().unwrap();
+        let password = pg.generate_one().unwrap();
+        let admin_uri = match url::Url::parse(&self.redis_uri) {
+            Ok(admin_uri) => admin_uri,
+            Err(e) => return Err(format!("URI Parse Error: {}", e)),
+        };
+        let user_uri = match admin_uri.port() {
+            Some(port) => format!(
+                "{}://{}:{}@{}:{}/",
+                admin_uri.scheme(),
+                username,
+                password,
+                admin_uri.host_str().unwrap(),
+                port
+            ),
+            None => format!(
+                "{}://{}:{}@{}/",
+                admin_uri.scheme(),
+                username,
+                password,
+                admin_uri.host_str().unwrap(),
+            ),
+        };
+        let mut con = self.connect().await?;
+        match con
+            .acl_setuser_rules::<&str, ()>(
+                &username,
+                &[
+                    Rule::On,
+                    Rule::AddPass(password.to_owned()),
+                    Rule::Pattern(format!("{}:*", username).to_owned()),
+                    Rule::AddCategory("stream".to_owned()),
+                ],
+            )
+            .await
+        {
+            Ok(_) => {}
+            Err(e) => return Err(format!("RedisStream ACL SETUSER Error: {}", e)),
+        }
+        Ok(user_uri)
     }
 
     async fn delete_user_account(&self, user_uri: &str) -> Result<(), String> {
+        let user_uri = match url::Url::parse(&user_uri) {
+            Ok(uri) => uri,
+            Err(e) => return Err(format!("URI Parse Error: {}", e)),
+        };
+        let user_name = user_uri.username();
+        let mut con = self.connect().await?;
+        match con.acl_deluser::<&str, ()>(&[user_name]).await {
+            Ok(_) => {}
+            Err(e) => return Err(format!("RedisStream ACL DELUSER Error: {}", e)),
+        };
         Ok(())
     }
 
     async fn delete_all_accounts(&self) -> Result<(), String> {
+        let mut con = self.connect().await?;
+        match redis::cmd("FLUSHALL")
+            .query_async::<Connection, ()>(&mut con)
+            .await
+        {
+            Ok(_) => {}
+            Err(e) => return Err(format!("RedisStream FLUSHALL Error: {}", e)),
+        }
+        let mut users: Vec<String> = match con.acl_users().await {
+            Ok(users) => users,
+            Err(e) => return Err(format!("RedisStream ACL USERS Error: {}", e)),
+        };
+        let my_user_name: String = match con.acl_whoami().await {
+            Ok(user_name) => user_name,
+            Err(e) => return Err(format!("RedisStream ACL WHOAMI Error: {}", e)),
+        };
+        users.retain(|x| x != &my_user_name);
+        match con.acl_deluser::<String, ()>(&users).await {
+            Ok(_) => {}
+            Err(e) => return Err(format!("RedisStream ACL DELUSER Error: {}", e)),
+        };
         Ok(())
     }
 
     async fn create_queue(&self, user_uri: &str, queue_name: &str) -> Result<String, String> {
-        let mut con = self.connect(user_uri).await?;
+        let mut con = self.connect().await?;
         let queue_name = if queue_name.is_empty() {
             passwords::PasswordGenerator::new()
                 .length(16)
@@ -59,7 +139,7 @@ impl crate::mq::common::MQ for RedisStream {
     }
 
     async fn delete_queue(&self, user_uri: &str, queue_name: &str) -> Result<(), String> {
-        let mut con = self.connect(user_uri).await?;
+        let mut con = self.connect().await?;
         match con
             .xgroup_destroy::<&str, &str, ()>(&queue_name, &queue_name)
             .await
@@ -96,7 +176,7 @@ impl crate::mq::common::MQ for RedisStream {
         if !routing_table.contains_key(key) {
             return Ok(());
         }
-        let mut con = self.connect(user_uri).await?;
+        let mut con = self.connect().await?;
         for queue_name in routing_table.get(key).unwrap() {
             match con
                 .xadd::<&str, &str, &str, &[u8], ()>(queue_name, "*", &[("payload", payload)])
@@ -111,8 +191,8 @@ impl crate::mq::common::MQ for RedisStream {
 }
 
 impl RedisStream {
-    async fn connect(&self, user_uri: &str) -> Result<Connection, String> {
-        let client = match redis::Client::open("redis://127.0.0.1/") {
+    async fn connect(&self) -> Result<Connection, String> {
+        let client = match redis::Client::open(&*self.redis_uri) {
             Ok(client) => client,
             Err(e) => return Err(format!("Redis Connection Error: {}", e)),
         };
